a2834744200081ab1faa5079bf3bec8baa772649
khaviettrinh <trinhb1910164@student.ctu.edu.vn>
2023-02-04 18:16:23 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require("express");
+const contactsRouter = require("./app/routes/contact.route");
 const cors = require("cors");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-app.get("/", (req, res) => {
-    res.json({ message: "Welcome to contact book application." });
-});
+app.use("/api/contacts", contactsRouter);
 
 module.exports = app;
+
